8826fbef915f4f3137f5f450f76ff9d3b6d92e55
Gergő Gázsa <ggary.dev@gmail.com>
2017-03-24 00:01:06 +0100

    GettingStarted : HelloWorldApp created.
    This application displays a specific string to the stdout.

---
+++ 01.GettingStarted/src/helloworldapp/HelloWorldApp.java
@@ -0,0 +1,18 @@
+package helloworldapp;
+
+/**
+ * 
+ * The HelloWorldApp class implements an application which 
+ * simply prints out a message to the stdout (standard output).
+ * 
+ * @author Geryke
+ *
+ */
+public class HelloWorldApp {
+	/**
+	 * @param args - the command line arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hey World! We're fabulous! ^^"); // displays the provided string
+	}
+}
